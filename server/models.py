0ddd69f80bd8298a7f7087c64c0c8831e4251e2c
Cristina <kamuthucristina@gmail.com>
2025-06-23 16:05:50 +0300

    added password authentication

--- server/models.py
+++ server/models.py
@@ -13,7 +13,18 @@ class User(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username =db.Column(db.String, unique=True ,nullable=False)
-    password_hash = db.Column(db.String, nullable=False)
+    __password_hash = db.Column(db.String, nullable=False)
+
+@hybrid_property
+def password_hash(self):
+    return self._password_hash
+
+@password_hash.setter
+def password_hash(self, password):
+    self._password_hash = bcrypt.generate_password_hash(password.encode()).decode()
+
+def authenticate(self, password):
+    return self._password_hash and bcrypt.check_password_hash(self._password_hash, password.encode())
 
 
 class Guest(db.Model, SerializerMixin):
@@ -22,8 +33,7 @@ class Guest(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     occupation = db.Column (db.String)
-
-    episodes = relationship('Episode' ,back_populates='guest')
+    episodes = relationship('Episode',back_populates='guest')
 
 
 class Episode(db.Model, SerializerMixin):
@@ -42,6 +52,8 @@ class Appearance(db.Model, SerializerMixin):
     rating = db.Column(db.Integer)
     guest_id = db.Column(db.Integer, ForeignKey('guests.id'))
     episode_id = db.Column(db.Integer, ForeignKey('episodes.id'))
+
+
 
 
     
